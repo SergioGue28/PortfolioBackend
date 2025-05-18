7c62ef9573695a204bb256eb1668e2a88d5803c8
SergioGue28 <sergioco2807@gmail.com>
2025-05-18 13:46:12 -0500

    Fix(corrsCongig) Change code (POOOBE)

--- src/main/java/com/apiportfolio/backendportfolio/config/CorsConfig.java
+++ src/main/java/com/apiportfolio/backendportfolio/config/CorsConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
@@ -15,21 +14,14 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:3000",
-                "https://portfolio-one-self-63.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://portfolio-one-self-63.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
-
         return source;
     }
 
-    @Bean
-    public CorsFilter corsFilter() {
-        return new CorsFilter(corsConfigurationSource());
-    }
 }
