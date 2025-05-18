c897cfcafdd385a8fb901f1031fa26f639191223
SergioGue28 <sergioco2807@gmail.com>
2025-05-17 23:02:27 -0500

    Feat(CorsConfig) Access for ftontend by vercel (POFOO-01)

--- src/main/java/com/apiportfolio/backendportfolio/config/CorsConfig.java
+++ src/main/java/com/apiportfolio/backendportfolio/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://moneylender-chi.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://portfolio-one-self-63.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
