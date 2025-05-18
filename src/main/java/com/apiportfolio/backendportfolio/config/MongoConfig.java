7a9a4c28f5d24363c7e6e336b61b5dc4d2cbbf6f
SergioGue28 <sergioco2807@gmail.com>
2025-05-17 21:41:27 -0500

    Feat(mongoConfig) Changed code in this file (POFOOBE-01)

--- src/main/java/com/apiportfolio/backendportfolio/config/MongoConfig.java
+++ src/main/java/com/apiportfolio/backendportfolio/config/MongoConfig.java
@@ -1,28 +1,29 @@
 package com.apiportfolio.backendportfolio.config;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
+public class MongoConfig {
 
-    private final Dotenv dotenv = Dotenv.load();
+    @Value("${MONGODB_URI}")
+    private String mongoUri;
 
-    @Override
-    protected String getDatabaseName() {
-        return dotenv.get("DATABASE_NAME");
+    @Value("${DATABASE_NAME}")
+    private String databaseName;
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
     }
 
     @Bean
-    public MongoClientSettings mongoClientSettings() {
-        ConnectionString connectionString = new ConnectionString(dotenv.get("MONGODB_URI"));
-        return MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .build();
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), databaseName);
     }
 }
